38dd59a5f4be493e3cd6f282af8b242d48ce797d
Madeline Schimenti <madelinelise@gmail.com>
2015-09-09 18:56:33 -0400

    Added Employee Class
    -Created an Employee Class.
    -Add three variables and properties.
    -Next I need to create a constructor.

---
+++ Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace madeline_schimenti_A2
+{
+    class Employee
+    {
+        string firstName;
+        string lastName;
+        decimal yearSalary;
+
+        
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                this.firstName= value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public decimal YearSalary
+        {
+            get
+            {
+                return yearSalary;
+            }
+            set
+            {
+                this.yearSalary = value;
+            }
+        }
+
+
+    } // end Employee Class
+}
